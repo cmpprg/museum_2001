fa03cc6a0e0cd73bff78aafb2724450529ea390c
Ryan Camp <cmpprg@icloud.com>
2020-02-19 11:00:03 -0700

    Create draw lottery winner/ announce lotter winner methods without tests

--- lib/museum.rb
+++ lib/museum.rb
@@ -34,6 +34,17 @@ class Museum
       patron.interests.include?(exhibit.name) &&
       patron.spending_money < exhibit.cost
     end
-  
   end
+
+  def draw_lottery_winner(exhibit)
+    ticket_lottery_contestants(exhibit).sample.name
+  end
+
+  def announce_lotter_winner(exhibit)
+    return "No winners for this lottery" if draw_lottery_winner(exhibit) == nil
+    "#{draw_lottery_winner(exhibit)} has won the #{exhibit.name} lottery!"
+  end
+
+
+
 end
