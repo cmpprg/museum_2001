e5ca7f8078ca161e3fc1fc8bbd9db0954bd0fcc8
Ryan Camp <cmpprg@icloud.com>
2020-02-19 09:34:58 -0700

    Create test to add exhibits for museum

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -1,6 +1,7 @@
 require "minitest/autorun"
 require "minitest/pride"
 require "./lib/museum"
+require "./lib/exhibit"
 
 class MuseumTest < Minitest::Test
   def test_it_exists
@@ -14,5 +15,20 @@ class MuseumTest < Minitest::Test
 
     assert_equal "Denver Museum of Nature and Science", dmns.name
     assert_equal [], dmns.exhibits
+  end
+
+  def test_it_can_add_exhibits
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    assert_equal [], dmns.exhibits
+
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    assert_equal [gems_and_minerals, dead_sea_scrolls, imax], dmns.exhibits
   end
 end
