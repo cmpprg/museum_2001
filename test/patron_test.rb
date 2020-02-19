df4f03d727ad65f5e5ef2d320a660c719bdc9c9a
Ryan Camp <cmpprg@icloud.com>
2020-02-19 09:17:05 -0700

    Create attribute test for patron

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -8,4 +8,13 @@ class PatronTest < Minitest::Test
 
     assert_instance_of Patron, patron_1
   end
+
+  def test_it_can_read_attributes
+    patron_1 = Patron.new("Bob", 20)
+
+    assert_equal "Bob", patron_1.name
+    assert_equal 20, patron_1.spending_money
+    assert_equal [], patron_1.interests
+
+  end
 end
