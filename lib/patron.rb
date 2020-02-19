fef5bf2b053affa440da0791f3e204f15cfbecbf
Ryan Camp <cmpprg@icloud.com>
2020-02-19 09:22:40 -0700

    Create add interest method for patron

--- lib/patron.rb
+++ lib/patron.rb
@@ -4,6 +4,10 @@ class Patron
   def initialize(name, spending_money)
     @name = name
     @spending_money = spending_money
-    @interests = [] 
+    @interests = []
+  end
+
+  def add_interest(interest)
+    @interests << interest
   end
 end
